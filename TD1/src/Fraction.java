f244616692c5b044f124ff36285e3163a66b6310
Clément Coulon <cle.coulon@gmail.com>
2020-10-16 13:55:53 +0200

    Fraction add toString

--- TD1/src/Fraction.java
+++ TD1/src/Fraction.java
@@ -1,4 +1,17 @@
 
 public class Fraction {
 
+	private int numerator, denominator; 
+	
+
+	public Fraction() {
+		numerator = denominator = 0; 
+	}
+
+
+	@Override
+	public String toString() {
+		return "Fraction [numerator=" + numerator + ", denominator=" + denominator + "]";
+	}
+	
 }
--- TD1/src/Main.java
+++ TD1/src/Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
 	public static void main(String [] args) {
-		System.out.println("Hello world");
+		
+		Fraction f = new Fraction(); 
+		System.out.println(f);
+		
+		//System.out.println("Hello world");
 	}
 
 }
